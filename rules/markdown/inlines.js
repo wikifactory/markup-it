88c16eb4895fc96031fde1dce24ea381bdf387bf
Samy Pessé <samypesse@gmail.com>
2016-03-12 20:38:41 +0100

    Add base for table generation

--- rules/markdown/inlines.js
+++ rules/markdown/inlines.js
@@ -158,5 +158,29 @@ module.exports = [
         .toText(function(text, entity) {
             if (!entity.data.id) return '';
             return '{#' + entity.data.id + '}';
-        })
+        }),
+
+    // ---- TABLE ----
+    markup.Rule(markup.INLINES.TABLE_HEADER)
+        .toText(function(text, entity) {
+            var align = entity.data.align;
+
+            return align.reduce(function(align) {
+                if (align == 'right') {
+                    return '---: |';
+                } else if (align == 'left') {
+                    return ':--- |';
+                } else if (align == 'center') {
+                    return ':---: |';
+                } else  {
+                    return '--- |';
+                }
+            }, text + '\n|');
+        }),
+    markup.Rule(markup.INLINES.TABLE_BODY)
+        .toText('%s'),
+    markup.Rule(markup.INLINES.TABLE_ROW)
+        .toText('| %s\n'),
+    markup.Rule(markup.INLINES.TABLE_CELL)
+        .toText('%s |'),
 ];
--- rules/markdown/table.js
+++ rules/markdown/table.js
@@ -19,11 +19,13 @@ function TableRow(cells) {
     );
 }
 
-function TableHeader() {
+function TableHeader(align) {
     return markup.Entity(
         markup.INLINES.TABLE_HEADER,
         markup.Entity.MUTABLE,
-        {}
+        {
+            align: align
+        }
     );
 }
 
@@ -63,7 +65,7 @@ function Table(header, align, rows) {
         );
     }
 
-    result = pushEntity(0, result, pushRow(0, header), TableHeader());
+    result = pushEntity(0, result, pushRow(0, header), TableHeader(align));
 
 
     var tableBody = rows.reduce(function(text, cells) {
@@ -77,11 +79,45 @@ function Table(header, align, rows) {
     };
 }
 
+// Detect alignement per column
+function mapAlign(align) {
+    return align.map(function(s) {
+        if (/^ *-+: *$/.test(s)) {
+            return 'right';
+        } else if (/^ *:-+: *$/.test(s)) {
+            return 'center';
+        } else if (/^ *:-+ *$/.test(s)) {
+            return 'left';
+        } else {
+            return null;
+        }
+    });
+}
+
+// SPlit rows into cells
+function splitRows(rows) {
+    return rows = rows.map(function(row) {
+        return row
+            .replace(/^ *\| *| *\| *$/g, '')
+            .split(/ *\| */);
+    });
+}
+
 var blockRule = markup.Rule(markup.BLOCKS.TABLE)
 
     // table no leading pipe (gfm)
     .regExp(reBlock.tables.nptable, function(match) {
+        var header = match[1].replace(/^ *| *\| *$/g, '').split(/ *\| */);
+        var align = match[2].replace(/^ *|\| *$/g, '').split(/ *\| */);
+        var rows = match[3].replace(/\n$/, '').split('\n');
 
+        // Align for columns
+        align = mapAlign(align);
+
+        // Split each row into cells
+        rows = splitRows(rows);
+
+        return Table(header, align, rows);
     })
 
     // normal table
@@ -91,32 +127,19 @@ var blockRule = markup.Rule(markup.BLOCKS.TABLE)
         var rows = match[3].replace(/(?: *\| *)?\n$/, '').split('\n').slice(0);
 
         // Align for columns
-        align = align.map(function(s) {
-            if (/^ *-+: *$/.test(s)) {
-                return 'right';
-            } else if (/^ *:-+: *$/.test(s)) {
-                return 'center';
-            } else if (/^ *:-+ *$/.test(s)) {
-                return 'left';
-            } else {
-                return null;
-            }
-        });
+        align = mapAlign(align);
 
         // Split each row into cells
-        rows = rows.map(function(row) {
-            return row
-                .replace(/^ *\| *| *\| *$/g, '')
-                .split(/ *\| */);
-        });
+        rows = splitRows(rows);
 
         return Table(header, align, rows);
 
     })
 
     // Output table as text
-    .toText(function() {
-
+    .toText(function(inner) {
+        console.log(inner)
+        return inner;
     });
 
 module.exports = {
