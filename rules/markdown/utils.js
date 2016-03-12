e98ed85b0adffa2941fe549a947c8e4f99ac9b77
Samy Pessé <samypesse@gmail.com>
2016-03-12 10:01:45 +0100

    Correctly escape all markdown syntaxes

--- rules/markdown/utils.js
+++ rules/markdown/utils.js
@@ -1,16 +1,41 @@
+var escapeStringRegexp = require('escape-string-regexp');
+
+// Replacements for Markdown escaping
+var replacements = [
+    [ '*', '\\*' ],
+    [ '#', '\\#' ],
+    [ '/', '\\/' ],
+    [ '(', '\\(' ],
+    [ ')', '\\)' ],
+    [ '[', '\\[' ],
+    [ ']', '\\]' ],
+    [ '<', '&lt;' ],
+    [ '>', '&gt;' ],
+    [ '_', '\\_' ]
+];
+
 // Split a text into lines
 function splitLines(text) {
     return text.split(/\r?\n/);
 }
 
+// Build a regexp from a string
+function re(str) {
+    return new RegExp(escapeStringRegexp(str), 'g');
+}
+
 // Escape markdown syntax
 function escapeMarkdown(str) {
-    return str.replace(/\*/g, '\\*');
+    return replacements.reduce(function(text, repl) {
+        return text.replace(re(repl[0]), repl[1]);
+    }, str);
 }
 
 // Unescape markdown syntax
 function unescapeMarkdown(str) {
-    return str.replace(/\\\\*/g, '\\*');
+    return replacements.reduce(function(text, repl) {
+        return text.replace(re(repl[1]), repl[0]);
+    }, str);
 }
 
 module.exports = {
