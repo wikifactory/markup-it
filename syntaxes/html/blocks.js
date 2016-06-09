ebae6b20dbb1bad6b10c0f553350e62d4b16bc5e
Soreine <soreine.plume@gmail.com>
2016-06-09 09:55:58 +0200

    Tweak for code blocks

--- syntaxes/html/blocks.js
+++ syntaxes/html/blocks.js
@@ -57,6 +57,6 @@ module.exports = [
                 attr = ' class="lang-' + token.data.syntax +'"';
             }
 
-            return '<pre><code' + attr + '>' + utils.escape(text) + '</code></pre>\n\n';
+            return '<pre><code' + attr + '>' + utils.escape(text) + '</code></pre>\n';
         })
 ];
