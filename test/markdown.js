091f16022ed7ac6694e4bee04f88093746b53aa5
Soreine <soreine.plume@gmail.com>
2016-06-09 10:43:38 +0200

    Don't optimize tests early...

--- test/markdown.js
+++ test/markdown.js
@@ -15,43 +15,61 @@ var ONLY = argv.testOnlyFixture;
 describe('Markdown', function() {
 
     describe('Fixtures', function() {
-
-        function testToHTML(fixture) {
-            fixture = resultHTML(fixture);
-            // Our fixtures files all end with trailing newline
-            (fixture.resultHTML).should.be.html(fixture.expectedHTML);
-            return fixture;
-        }
-
-        function readFixture(filename) {
-            var htmlFilePath = path.basename(filename, '.md') + '.html';
-
-            var sourceMD = fs.readFileSync(path.resolve(FIXTURES, filename), 'utf8');
-            var expectedHTML = fs.readFileSync(path.resolve(FIXTURES, htmlFilePath), 'utf8');
-
-            return {
-                content: markdown.toContent(sourceMD),
-                sourceMD: sourceMD,
-                expectedHTML: expectedHTML
-            };
-        }
-
         var files = fs.readdirSync(FIXTURES);
         files.forEach(function(file) {
             if (path.extname(file) !== '.md') return;
             if (ONLY && path.basename(file, '.md') !== ONLY) return;
 
-            it(file, function() {
+            describe(file, function() {
                 var fixture = readFixture(file);
-                fixture = testToHTML(fixture);
+
+                it('MD -> HTML', function () {
+                    testMdToHtml(fixture);
+                });
+
+                it('HTML -> MD', function () {
+                    testHtmlToMd(fixture);
+                });
+
+                it('MD -> HTML -> MD', function () {
+                    testIdentityMd(fixture);
+                });
             });
         });
     });
 });
 
-function resultHTML(fixture) {
-    if (!fixture.resultHTML) {
-        fixture.resultHTML = html.toText(fixture.content);
-    }
-    return fixture;
+
+function testMdToHtml(fixture) {
+    var content = markdown.toContent(fixture.sourceMd);
+    var resultHtml = html.toText(content);
+    (resultHtml).should.be.html(fixture.sourceHtml);
+}
+
+function testHtmlToMd(fixture) {
+    var content = html.toContent(fixture.sourceHtml);
+    var resultMd = markdown.toText(content);
+    (resultMd).should.equal(fixture.sourceMd);
+}
+
+function testIdentityMd(fixture) {
+    // MD to HTML
+    var mdToContent = markdown.toContent(fixture.sourceMd);
+    var resultHtml = html.toText(mdToContent);
+    // HTML to MD
+    var htmlToContent = html.toContent(resultHtml);
+    var resultMd = markdown.toText(htmlToContent);
+    (resultMd).should.equal(fixture.sourceMd);
+}
+
+function readFixture(filename) {
+    var htmlFilePath = path.basename(filename, '.md') + '.html';
+
+    var sourceMd = fs.readFileSync(path.resolve(FIXTURES, filename), 'utf8');
+    var sourceHtml = fs.readFileSync(path.resolve(FIXTURES, htmlFilePath), 'utf8');
+
+    return {
+        sourceMd: sourceMd,
+        sourceHtml: sourceHtml
+    };
 }
