8cf7dc3576114edc00550d3d10216bab235b09f7
Samy Pesse <samypesse@gmail.com>
2016-06-21 10:14:54 +0200

    Fix comment in heading rule

--- syntaxes/markdown/heading.js
+++ syntaxes/markdown/heading.js
@@ -1,7 +1,12 @@
 var reHeading = require('./re/heading');
 var markup = require('../../');
 
-// Parse inner text of header to extract ID entity
+/**
+ * Parse inner text of header to extract ID entity
+ * @param  {ParsingState} state
+ * @param  {String} text
+ * @return {TokenLike}
+ */
 function parseHeadingText(state, text) {
     var id, match;
 
@@ -24,21 +29,30 @@ function parseHeadingText(state, text) {
     };
 }
 
-// Generator for HEADING_X rules
+/**
+ * Generator for HEADING_X rules
+ * @param  {Number} level
+ * @return {Rule}
+ */
 function headingRule(level) {
     var prefix = Array(level + 1).join('#');
 
     return markup.Rule(markup.BLOCKS['HEADING_' + level])
+
+        // Normal heading like
         .regExp(reHeading.normal, function(state, match) {
             if (match[1].length != level) return null;
             return parseHeadingText(state, match[2]);
         })
+
+        // Line heading
         .regExp(reHeading.line, function(state, match) {
             var matchLevel = (match[2] === '=')? 1 : 2;
             if (matchLevel != level) return null;
 
             return parseHeadingText(state, match[1]);
         })
+
         .toText(function (state, token) {
             var data = token.getData();
             var innerContent = state.renderAsInline(token);
