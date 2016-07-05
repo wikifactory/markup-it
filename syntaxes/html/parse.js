45aa0333cabfcadb9838117652afe3a942c4c55a
Samy Pessé <samypesse@gmail.com>
2016-07-05 10:43:17 +0200

    Add base for html parsing

--- syntaxes/html/parse.js
+++ syntaxes/html/parse.js
@@ -21,6 +21,10 @@ var TAGS_TO_TYPE = {
     tr:             MarkupIt.BLOCKS.TR_ROW,
     td:             MarkupIt.BLOCKS.TABLE_CELL,
 
+    ul:             MarkupIt.BLOCKS.LIST_UL,
+    ol:             MarkupIt.BLOCKS.LIST_OL,
+    li:             MarkupIt.BLOCKS.LIST_ITEM,
+
     b:              MarkupIt.STYLES.BOLD,
     strike:         MarkupIt.STYLES.STRIKETHROUGH,
     em:             MarkupIt.STYLES.ITALIC,
@@ -33,27 +37,46 @@ var TAGS_TO_TYPE = {
  * @return {List<Token>}
  */
 function htmlToTokens(str) {
-    var accuText = '';
     var result = [];
+    var stack = [];
+
+    function pushToLast(tok) {
+        if (stack.length > 0) {
+            stack[stack.length - 1] = stack[stack.length - 1].pushToken(tok);
+        } else {
+            result.push(tok);
+        }
+    }
 
     var parser = new htmlparser.Parser({
-        onopentag: function(name, attribs){
-
-        },
-        ontext: function(text){
-            accuText += text;
-        },
-        onclosetag: function(tagname){
-            var type = TAGS_TO_TYPE[tagname];
+        onopentag: function(tagName, attribs) {
+            var type = TAGS_TO_TYPE[tagName];
             if (!type) {
                 return;
             }
 
             var token = MarkupIt.Token.create(type, {
-                text: accuText
+                data: {}
             });
 
-            accuText = '';
+            stack.push(token);
+        },
+        ontext: function(text) {
+            if (!text.trim()) {
+                return;
+            }
+
+            var textNode = MarkupIt.Token.createText(text);
+            pushToLast(textNode);
+        },
+        onclosetag: function(tagName) {
+            var type = TAGS_TO_TYPE[tagName];
+            if (!type) {
+                return;
+            }
+
+            var lastToken = stack.pop();
+            pushToLast(lastToken);
         }
     }, {
         decodeEntities: true
