2c1079563dae816c05d1defcb6568ce1983da2a8
Samy Pesse <samypesse@gmail.com>
2016-10-28 00:04:53 +0200

    Export constants

---
+++ src/constants/inlines.js
@@ -0,0 +1,8 @@
+module.exports = {
+    HTML:         'HTML',
+    LINK:         'link',
+    IMAGE:        'image',
+    FOOTNOTE_REF: 'footnote-ref',
+    MATH:         'math',
+    TEMPLATE:     'template'
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 const State = require('./models/state');
 const Node = require('./models/node');
 
+const MARKS = require('./constants/marks');
+const BLOCKS = require('./constants/blocks');
+const INLINES = require('./constants/inlines');
+
 module.exports = {
     State,
-    Node
+    Node,
+    MARKS,
+    BLOCKS,
+    INLINES
 }
