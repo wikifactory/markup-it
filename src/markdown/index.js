b6a153783d13df3f6256d9a84854029c69df2d86
Samy Pesse <samypesse@gmail.com>
2016-10-29 12:40:49 +0200

    Add bold and italic rules

--- src/markdown/index.js
+++ src/markdown/index.js
@@ -1,9 +1,14 @@
 const heading = require('./heading');
 const paragraph = require('./paragraph');
 const codeBlock = require('./code-block');
+const bold = require('./bold');
+const italic = require('./italic');
 
 module.exports = [
     codeBlock,
     heading,
-    paragraph
+    paragraph,
+    // Marks
+    italic,
+    bold
 ];
