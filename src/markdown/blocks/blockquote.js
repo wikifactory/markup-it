4dba6a87ac09fba8c3662372b384ab69ee4ce5a0
Samy Pesse <samypesse@gmail.com>
2016-11-11 22:50:07 +0100

    Add parsing of blockquote

---
+++ src/markdown/blocks/blockquote.js
@@ -0,0 +1,44 @@
+const splitLines = require('split-lines');
+const { Serializer, Deserializer, Block, BLOCKS } = require('../../');
+const reBlock = require('../re/block');
+
+/**
+ * Serialize a blockquote node to markdown
+ * @type {Serializer}
+ */
+const serialize = Serializer()
+    .matchType(BLOCKS.BLOCKQUOTE)
+    .then((state) => {
+        const node = state.peek();
+        const inner = state.use('block').serialize(node.nodes);
+        const lines = splitLines(inner.trim());
+
+        const output = lines
+            .map(function(line) {
+                return '> ' + line;
+            })
+            .join('\n');
+
+        return state
+            .shift()
+            .write(`${output}\n\n`);
+    });
+
+/**
+ * Deserialize a blockquote to a node.
+ * @type {Deserializer}
+ */
+const deserialize = Deserializer()
+    .matchRegExp(reBlock.blockquote, (state, match) => {
+        const inner = match[0].replace(/^ *> ?/gm, '').trim();
+        // TODO: set a prop isBlockquote
+        const nodes = state.use('block').deserialize(inner);
+        const node = Block.create({
+            type: BLOCKS.BLOCKQUOTE,
+            nodes
+        });
+
+        return state.push(node);
+    });
+
+module.exports = { serialize, deserialize };
--- src/markdown/blocks/index.js
+++ src/markdown/blocks/index.js
@@ -1,9 +1,13 @@
 const heading = require('./heading');
 const paragraph = require('./paragraph');
 const codeBlock = require('./code');
+const blockquote = require('./blockquote');
+const unstyled = require('./unstyled');
 
 module.exports = [
+    blockquote,
     codeBlock,
     heading,
-    paragraph
+    paragraph,
+    unstyled
 ];
