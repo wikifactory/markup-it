c5c79acc5f9a12089733eb5eb9cf8b1c18f53f24
Samy Pessé <samypesse@gmail.com>
2016-11-14 11:00:21 +0100

    Add parsing of footnote refs

--- src/markdown/inlines/index.js
+++ src/markdown/inlines/index.js
@@ -2,9 +2,11 @@ const text = require('./text');
 const code = require('./code');
 const bold = require('./bold');
 const italic = require('./italic');
+const footnote = require('./footnote');
 const strikethrough = require('./strikethrough');
 
 module.exports = [
+    footnote,
     // Code mark should be applied before everything else
     code,
     // Bold should be before italic
