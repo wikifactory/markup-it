3567eaa4d2d0634c51fd39f4a19e04b22b0a3755
Samy Pessé <samypesse@gmail.com>
2016-11-15 13:29:19 +0100

    Add basic parsing for lists

--- src/markdown/blocks/list.js
+++ src/markdown/blocks/list.js
@@ -1,7 +1,7 @@
 const splitLines = require('split-lines');
 const leftPad = require('left-pad');
 const { Serializer, Deserializer, Block, BLOCKS } = require('../../');
-const reBlock = require('../re/block');
+const reList = require('../re/block').list;
 
 /**
  * Serialize a list to markdown
@@ -26,8 +26,73 @@ const serialize = Serializer()
  * @type {Deserializer}
  */
 const deserialize = Deserializer()
-    .matchRegExp(reBlock.paragraph, (state, match) => {
+    .matchRegExp(reList.block, (state, match) => {
+        const rawList = match[0];
+        const bull = match[2];
+        const ordered = bull.length > 1;
 
+        const type = ordered ? BLOCKS.OL_LIST : BLOCKS.UL_LIST;
+
+        let item, loose, next = false;
+
+        let lastIndex = 0;
+        const nodes = [];
+        let rawItem, textItem, space;
+        const items = [];
+
+        // Extract all items
+        reList.item.lastIndex = 0;
+        while ((item = reList.item.exec(rawList)) !== null) {
+            rawItem = rawList.slice(lastIndex, reList.item.lastIndex);
+            lastIndex = reList.item.lastIndex;
+
+            items.push([item, rawItem]);
+        }
+
+        for (let i = 0; i < items.length; i++) {
+            item = items[i][0];
+            rawItem = items[i][1];
+
+            // Remove the list item's bullet
+            // so it is seen as the next token.
+            textItem = item[0];
+            space = textItem.length;
+            textItem = textItem.replace(/^ *([*+-]|\d+\.) +/, '');
+
+            // Outdent whatever the
+            // list item contains. Hacky.
+            if (~textItem.indexOf('\n ')) {
+                space -= textItem.length;
+                textItem =  textItem.replace(new RegExp('^ {1,' + space + '}', 'gm'), '');
+            }
+
+            // Determine whether item is loose or not.
+            // Use: /(^|\n)(?! )[^\n]+\n\n(?!\s*$)/
+            // for discount behavior.
+            loose = next || /\n\n(?!\s*$)/.test(textItem);
+            if (i !== items.length - 1) {
+                next = textItem.charAt(textItem.length - 1) === '\n';
+                if (!loose) loose = next;
+            }
+
+            const nodeItem = Block.create({
+                type: BLOCKS.LIST_ITEM,
+                nodes: (loose ? state.setProp('looseList', state.depth) : state)
+                    .use('block')
+                    .deserialize(textItem)
+                // data: { loose }
+            });
+
+
+            nodes.push(nodeItem);
+        }
+
+        const listBlock = Block.create({
+            type,
+            nodes
+        });
+
+        return state.push(listBlock);
     });
 
 /**
@@ -49,7 +114,7 @@ function renderItemToText(state, list, item, index) {
     const lines = splitLines(inner);
     const head = lines[0];
     const body = lines.slice(1);
-    const spaces = leftPad('X', bullet.length, ' ');
+    const spaces = leftPad('', bullet.length, ' ');
 
     const rest = body.map(line => line ? `${spaces} ${line}` : '').join('\n');
     const eol = rest ? '' : '\n';
