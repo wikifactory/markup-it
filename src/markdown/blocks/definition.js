12eeaf98d5e156b4e6d902d9617f8af407d3b875
Samy Pessé <samypesse@gmail.com>
2016-11-25 16:18:52 +0100

    Base for parsing all definitions

--- src/markdown/blocks/definition.js
+++ src/markdown/blocks/definition.js
@@ -1,12 +1,12 @@
 const { Map } = require('immutable');
-const { Deserializer } = require('../../');
+const { Deserializer, State } = require('../../');
 const reBlock = require('../re/block');
 
 /**
  * Deserialize a definition.
  * @type {Deserializer}
  */
-const deserialize = Deserializer()
+const deserializeOnlyDef = Deserializer()
     .matchRegExp(reBlock.def, (state, match) => {
         if (state.depth > 1) {
             return;
@@ -24,6 +24,27 @@ const deserialize = Deserializer()
 
         // No node is pushed when parsing definition
         return state.setProp('refs', refs);
+    });
+
+/**
+ * Deserialize all definitions in a markdown document and store them as
+ * "refs" prop.
+ * @type {Deserializer}
+ */
+const deserialize = Deserializer()
+    .then(state => {
+        const { text, depth, nodes } = state;
+
+        // Apply it as first rule only
+        if (depth > 0 || nodes.size > 0) {
+            return;
+        }
+
+        console.log('parse definitions');
+
+        const parser = State({
+            blocks: [ { deserialize: deserializeOnlyDef } ]
+        });
     });
 
 module.exports = { deserialize };
